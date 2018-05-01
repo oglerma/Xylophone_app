f2716c4ff02a59ff764f571149427d5a78e1135d
O gee Lerma <oglerma@gmail.com>
2018-04-30 20:29:58 -0700

    Used AvFoundation library to use AVAudioPlayer in order to play sounds on the Xylophone app.

--- README.md
+++ README.md
@@ -2,11 +2,7 @@
 
 This base project is part of a Udemy course that i am taking and where we will have to add our own touch to make the app work. 
 - Ociel Lerma
-Learn to make iOS Apps with [The App Brewery](https://www.appbrewery.co) 📱 | Project Stub | (Swift 4.0/Xcode 9)
 
-Beginner: Download the starter project files as .zip and extract the files to your desktop.
-
-Pro: Git clone to your Xcode projects folder.
 
 ## Finished App
 <img src="https://github.com/londonappbrewery/Images/blob/master/Xylophone.png" width="400">
--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,21 +7,40 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController{
+import AVFoundation // This module allows playing sound.
+class ViewController: UIViewController, AVAudioPlayerDelegate{
     
-    // added comment to make a change.
+    var audioPlayer: AVAudioPlayer!
+    var soundArray = ["note1","note2","note3","note4","note5","note6","note7"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    func playSound(sound: String) {
+        
+        //Line 28 basically set up the location of where the sound is saved in our app and stores it
+        //into soundURL. Now soundUrl is an actual constant with a song inside.
+        let soundUrl = Bundle.main.url(forResource: sound, withExtension: "wav")
+        do {
+            //audioPlayer as we declared it but not assigned in line 13 will now take in that song.
+            //the reason it is on a try method is because we want to make sure that actual sound exsists.
+            //if it doesnt exsist then we will leave the try method and the code knows to go to a catch
+            //and display the error.
+            audioPlayer = try AVAudioPlayer(contentsOf: soundUrl!)
+            
+        }
+        catch{
+            print(error)
+        }
+        audioPlayer.play()
+    }
 
     @IBAction func notePressed(_ sender: UIButton) {
+        let selectedSoundFileName: String = soundArray[sender.tag - 1]
         
-        
-        
+        playSound(sound: selectedSoundFileName)
     }
     
   
